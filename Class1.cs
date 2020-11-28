5607f956e6261c1cb3abcc6d57cb51e35c3fd10f
Zac Gerhardy <zac.gerhardy@gmail.com>
2020-11-27 21:17:14 -0600

    Initial Checkin

---
+++ Class1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        -path    { uint[32]}
+        uint[]
+[0] 102 uint
+[1] 83  uint
+[2] 52  uint
+[3] 181 uint
+[4] 60  uint
+[5] 198 uint
+[6] 30  uint
+[7] 13  uint
+[8] 199 uint
+[9] 79  uint
+[10]    94  uint
+[11]    110 uint
+[12]    72  uint
+[13]    121 uint
+[14]    143 uint
+[15]    0   uint
+[16]    0   uint
+[17]    0   uint
+[18]    0   uint
+[19]    0   uint
+[20]    0   uint
+[21]    0   uint
+[22]    0   uint
+[23]    0   uint
+[24]    0   uint
+[25]    0   uint
+[26]    0   uint
+[27]    0   uint
+[28]    0   uint
+[29]    0   uint
+[30]    0   uint
+[31]    0   uint
+
+
+            -path    { uint[0x00000020]}
+        uint[]
+[0] 0x00000066  uint
+[1] 0x00000053  uint
+[2] 0x00000034  uint
+[3] 0x000000b5  uint
+[4] 0x0000003c  uint
+[5] 0x000000c6  uint
+[6] 0x0000001e  uint
+[7] 0x0000000d  uint
+[8] 0x000000c7  uint
+[9] 0x0000004f  uint
+[10]    0x0000005e  uint
+[11]    0x0000006e  uint
+[12]    0x00000048  uint
+[13]    0x00000079  uint
+[14]    0x0000008f  uint
+[15]    0x00000000  uint
+[16]    0x00000000  uint
+[17]    0x00000000  uint
+[18]    0x00000000  uint
+[19]    0x00000000  uint
+[20]    0x00000000  uint
+[21]    0x00000000  uint
+[22]    0x00000000  uint
+[23]    0x00000000  uint
+[24]    0x00000000  uint
+[25]    0x00000000  uint
+[26]    0x00000000  uint
+[27]    0x00000000  uint
+[28]    0x00000000  uint
+[29]    0x00000000  uint
+[30]    0x00000000  uint
+[31]    0x00000000  uint
+
+    }
+}
